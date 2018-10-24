71397c2cc0ae234a088bf49890cb5c0d2bb93dca
Arbnoob <iesalejandrorodriguezbanon@gmail.com>
2018-10-24 19:55:45 +0200

    were

--- src/main/java/com/iesemilidarder/base/data/Activities.java
+++ src/main/java/com/iesemilidarder/base/data/Activities.java
@@ -1,5 +1,8 @@
 package com.iesemilidarder.base.data;
 
 public class Activities extends Products{
-
+    private String desciption;
+    private double duration;
+    private String category;
+    private String disponibility;
 }
