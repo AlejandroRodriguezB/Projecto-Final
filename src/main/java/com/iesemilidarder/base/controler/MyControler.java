07c489ea67944bfeee2eef1e02a19b08dba801ad
Arbnoob <iesalejandrorodriguezbanon@gmail.com>
2018-10-31 19:14:01 +0100

    mal

--- src/main/java/com/iesemilidarder/base/controler/MyControler.java
+++ src/main/java/com/iesemilidarder/base/controler/MyControler.java
@@ -1,6 +1,5 @@
 package com.iesemilidarder.base.controler;
 
-import com.iesemilidarder.base.data.Products;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyControler {
 
+
+
     @RequestMapping("/Products")
-    public Products getCar(@RequestParam(value="name", defaultValue="Run Run") String name) {
-        Products aux = new Products() {
-            @Override
-            public int hashCode() {
-                return super.hashCode();
-            }
-        };
-        return aux;
+    public void getCar(@RequestParam(value="name", defaultValue="Run Run") String name) {
+
+        System.out.println("hola");
 
     }
 }
